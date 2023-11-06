248c95e12e3a47a1abd229aa10508c15820ad803
schnoog <schnoog@schnoog.eu>
2023-11-06 15:02:31 +0100

    function to get all followers of a person implemented

--- src/internal/atp_graph.php
+++ src/internal/atp_graph.php
@@ -9,13 +9,40 @@ $config['included'][] = "atp_graph.php";
  * @return bool 
  * @throws RestClientException 
  */
-function atp_graph_getfollowers($userhandle){
+function atp_graph_getfollowers($userhandle,$limit = 50,$cursor=""){
     global $config;
     $data = [
-        'actor' => $userhandle
+        'actor' => $userhandle,
+        'limit' => $limit,
+        
     ];
-    
+    if(strlen($cursor)> 4){
+        $data['cursor'] = $cursor;
+    }
     $retval = atp_get_data($config["nsid"]['getfollowers'],$data);
     return $retval;
 }
 
+
+/**
+ * atp_graph_getAllFollowers returns ALL the followers of a user handle in a handy array
+ * @param mixed $userhandle 
+ * @return bool 
+ * @throws RestClientException 
+ */
+function atp_graph_getAllFollowers($userhandle){
+        global $config;
+        $followers = array();
+        $singlecall = 100;
+        $cursor = "X";  
+        while(strlen($cursor) > 0){
+            $work = atp_graph_getfollowers($userhandle,$singlecall,$cursor);
+            $cursor = "";
+            if(isset($work['cursor']))$cursor = $work['cursor'];
+            $runfol = $work['followers'];
+            for($x = 0; $x < count($runfol);$x++){
+                $followers[] = $runfol[$x];
+            }
+        }
+        return $followers;
+}
